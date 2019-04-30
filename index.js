e489cee97946d26227f7929149bb1cd86e50b4c2
Siddhant Shukla <siddhantshukla356@gmail.com>
2019-04-30 06:46:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 function deleteFromObjectByKey(object, key){
-  delete object.key
+  return Object.assign({}, object, delete object.key)
 }
