981539a8e7e890a4c9c8396e0dfaecdc6b04ee5e
Siddhant Shukla <siddhantshukla356@gmail.com>
2019-04-30 06:31:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var recipes={};
-
 function updateObjectWithKeyAndValue(object, kye, value){
   return Object.assign({}, object, {kye: value})
+}
+function updateObjectWithKeyAndValue(object, key, value){
+  object[key]=value
+  return object
 }
